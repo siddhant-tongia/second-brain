67349720bf77f3a517a0210dfe59cc2be311b0d6
siddhant-tongia <tongiasiidhant42@gmail.com>
2026-07-03 14:19:44 +0530

    Added insert and delete function

--- app.py
+++ app.py
@@ -12,8 +12,10 @@ mydb = mysql.connector.connect(
 
 my_cursor = mydb.cursor()
 
-my_cursor.execute("SELECT * FROM resources")
-result = my_cursor.fetchall()
+title = input("Enter the Title Of Resource:")
+category = input("Enter the Category Of Resource:")
+description = input("Enter the Description Of Resource:")
+link = input("Enter the Link Of Resource:")
 
 query = "INSERT INTO resources (title, category, short_description, link) VALUES (%s, %s, %s, %s)"
 values = (title, category, description, link)
@@ -21,4 +23,15 @@ values = (title, category, description, link)
 my_cursor.execute(query, values)
 mydb.commit()
 
+
+resource_id = input("Enter the ID of the resource to delete: ")
+
+query = "DELETE FROM resources WHERE id = %s"
+values = (resource_id,)   # note the comma — why do you think this comma is necessary here?
+
+my_cursor.execute(query, values)
+mydb.commit()
+
+my_cursor.execute("SELECT * FROM resources")
+result = my_cursor.fetchall()
 print(result)
