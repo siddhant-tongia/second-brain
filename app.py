ab9bd37f556e7d9d9f138bb66f1a8efd0dd57554
siddhant-tongia <tongiasiidhant42@gmail.com>
2026-07-07 15:24:30 +0530

    added multiple query and extraction  option.

--- app.py
+++ app.py
@@ -17,16 +17,27 @@ def delete_multiple(id_list):
   my_cursor.execute(query, tuple(id_list))
   mydb.commit()
 
-def view_all():
-  my_cursor.execute("SELECT * FROM resources")
-  result = my_cursor.fetchall()
 
+def view_by_category(selected_category):
+  query = "SELECT * FROM resources WHERE Category = %s"
+  values = (selected_category,)
+  
+  my_cursor.execute(query,values)
+  result = my_cursor.fetchall()
+  display_results(result)
+
+def display_results(result):
   col = ("ID", "Title", "Category", "Description", "Link", "Created_at", "Updated_at")
 
   for row in result:
     for label, value in zip(col, row):
       print(f"{label}: {value}")
     print("---")
+def view_all():
+  my_cursor.execute("SELECT * FROM resources")
+  result = my_cursor.fetchall()
+  display_results(result)
+
 
 mydb = mysql.connector.connect(
   host = "localhost",
@@ -49,4 +60,19 @@ id_list = [int(x) for x in resource_ids.split(",")]
 
 delete_multiple(id_list)
 
-view_all()
+categories = ["AI Resources", "Business Ideas", "DSA Concepts", "Motivation", "Personal Growth"]
+
+for i,cat in enumerate (categories,start = 1):
+  print(f"{i}: {cat}")  
+
+choice = (input("Choose a categories (1-5) and if want to view all categories then type all")).lower()
+
+if(choice != "all"):
+  id_list = [int(num) for num in choice.split(",")]
+
+  for i in id_list:
+    selected_category = categories[i-1]
+    view_by_category(selected_category);
+
+else:
+  view_all()
